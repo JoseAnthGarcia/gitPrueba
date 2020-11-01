a3c5f6773e0e88bc72567372c071eada8a292cd0
Jose Garcia <garcia.josea@pucp.edu.pe>
2020-11-01 09:08:59 -0500

    se encapsulo dni a Profesor.java

--- src/Profesor.java
+++ src/Profesor.java
@@ -3,6 +3,14 @@ public class Profesor {
     private String edificio;
     private String dni;
 
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
     public String getNombre() {
         return nombre;
     }
